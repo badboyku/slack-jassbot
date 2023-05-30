a8fe0ccdc4d4ac1cb5e472be53be60caabbd23d3
Joey Atkinson <badboyKU@gmail.com>
2023-05-29 20:28:07 -0700

    Fix models.

--- src/db/models/ChannelModel.ts
+++ src/db/models/ChannelModel.ts
@@ -2,20 +2,17 @@
 import { model, Model, Schema } from 'mongoose';
 import type { ChannelDocType, ChannelHydratedDocument, ChannelMethods } from '@types';
 
-const { Boolean, Date, Number, ObjectId, String } = Schema.Types;
+const { Boolean, Number, String } = Schema.Types;
 
 type ChannelModel = Model<ChannelDocType, {}, ChannelMethods>;
 const schema = new Schema<ChannelDocType, ChannelModel, ChannelMethods, {}, {}, {}, {}, ChannelHydratedDocument>(
   {
-    _id: { type: ObjectId, index: true, unique: true },
     channelId: { type: String, index: true, unique: true },
     name: { type: String, default: '' },
     isMember: { type: Boolean, index: true, required: true, default: true },
     isPrivate: { type: Boolean, required: true, default: false },
     numMembers: { type: Number, index: true, required: true, default: 0 },
     members: { type: [String], index: true, default: [] },
-    createdAt: { type: Date },
-    updatedAt: { type: Date },
   },
   { collection: 'channels', timestamps: true },
 );
--- src/db/models/UserModel.ts
+++ src/db/models/UserModel.ts
@@ -4,19 +4,16 @@ import { crypto, dateTime } from '@utils';
 import type { DateTime } from 'luxon';
 import type { UserDocType, UserHydratedDocument, UserMethods } from '@types';
 
-const { Date, ObjectId, String } = Schema.Types;
+const { String } = Schema.Types;
 
 type UserModel = Model<UserDocType, {}, UserMethods>;
 const schema = new Schema<UserDocType, UserModel, UserMethods, {}, {}, {}, {}, UserHydratedDocument>(
   {
-    _id: { type: ObjectId, index: true, unique: true },
     userId: { type: String, index: true, unique: true },
     birthday: { type: String, default: '' },
     birthdayLookup: { type: String, index: true, default: '' },
     workAnniversary: { type: String, default: '' },
     workAnniversaryLookup: { type: String, index: true, default: '' },
-    createdAt: { type: Date },
-    updatedAt: { type: Date },
   },
   { collection: 'users', timestamps: true },
 );
