bacbfe819487828b2ab155f71ccb5d8c4778713d
Joey Atkinson <badboyKU@gmail.com>
2023-07-02 01:20:21 -0700

    add addUsersToChannels script

--- src/scripts/addUsersToChannels.ts
+++ src/scripts/addUsersToChannels.ts
@@ -1,94 +1,137 @@
-// /* istanbul ignore file */
-// import { faker } from '@faker-js/faker';
-// import { UserModel } from '@db/models';
-// import { dbJassbot } from '@db/sources';
-// import { channelService } from '@services';
-// import { config, logger } from '@utils';
-// import type { AnyBulkWriteOperation } from 'mongodb';
-// import type { ChannelData } from '@types';
-//
-// const getSampleUserIds = async (size = 1000): Promise<string[]> => {
-//   const userIds = [];
-//
-//   const pipeline = [{ $match: { userId: /^TEST/ } }, { $sample: { size } }];
-//   const options = { batchSize: 1000 };
-//   const cursor = UserModel.aggregate(pipeline).cursor(options);
-//   for await (const user of cursor) {
-//     userIds.push(user.userId);
-//   }
-//
-//   return userIds;
-// };
-//
-// const getMemberIds = (userIds: string[] = []): string[] => {
-//   const numMembers = faker.number.int({ min: 0, max: Math.floor(userIds.length / 2) });
-//   if (numMembers === 0) {
-//     return [];
-//   }
-//
-//   const sizeMaxIndex = userIds.length - 1;
-//   const halfNumMembers = Math.floor(numMembers / 2);
-//   const randIndex = faker.number.int({ min: 0, max: sizeMaxIndex });
-//
-//   let start = randIndex - halfNumMembers;
-//   let end = randIndex + halfNumMembers;
-//   if (start < 0) {
-//     start = 0;
-//     end = numMembers - 1;
-//   } else if (end > sizeMaxIndex) {
-//     start = sizeMaxIndex - numMembers - 1;
-//     end = sizeMaxIndex;
-//   }
-//
-//   return userIds.slice(start, end);
-// };
-//
-// (async () => {
-//   const numChannelsArg = Number(process.argv[2]) || undefined;
-//   const numChannels = Math.min(numChannelsArg || 1000, 5000);
-//   logger.info('scripts: addChannels called', { numChannelsArg, numChannels });
-//
-//   const { isConnected: isDbConnected } = await dbJassbot.connect();
-//   if (!isDbConnected) {
-//     logger.info('scripts: addChannels exiting', { error: 'Database failed to connect' });
-//
-//     process.exit(1);
-//   }
-//
-//   const sampleUserIds = await getSampleUserIds();
-//
-//   const ops: AnyBulkWriteOperation<ChannelData>[] = [];
-//   for (let i = 0; i < numChannels; i += 1) {
-//     const memberIds = getMemberIds(sampleUserIds);
-//
-//     const isMember = faker.datatype.boolean({ probability: 0.25 });
-//     if (isMember) {
-//       memberIds.push(config.slack.botUserId);
-//     }
-//
-//     ops.push({
-//       updateOne: {
-//         filter: { channelId: `TEST${faker.string.alphanumeric({ length: 7, casing: 'upper' })}` },
-//         update: {
-//           $set: {
-//             name: faker.word.words({ count: { min: 1, max: 8 } }).replaceAll(' ', '-'),
-//             isArchived: !isMember && faker.datatype.boolean({ probability: 0.02 }),
-//             isMember,
-//             isPrivate: faker.datatype.boolean({ probability: 0.4 }),
-//             numMembers: memberIds.length,
-//             memberIds,
-//             __v: 0,
-//           },
-//         },
-//         upsert: true,
-//       },
-//     });
-//   }
-//
-//   const results = await channelService.bulkWrite(ops);
-//   logger.info('scripts: addUsersToChannels completed', { results });
-//
-//   await dbJassbot.disconnect();
-//
-//   process.exit(0);
-// })();
+/* istanbul ignore file */
+import { faker } from '@faker-js/faker';
+import { dbJassbot } from '../db/sources';
+import { logger, mongodb } from '../utils';
+import scriptsHelper from './scriptsHelper';
+import type { AnyBulkWriteOperation, Collection, WriteError } from 'mongodb';
+import type { BulkWrite, Channel, User } from '@types';
+
+const getLogContext = (bulkWrite: BulkWrite) => {
+  const { result, error } = bulkWrite;
+
+  if (error) {
+    const { code, writeErrors, result: errResult } = error;
+    const errors: WriteError[] = writeErrors as WriteError[];
+    const { insertedCount, matchedCount, modifiedCount, deletedCount, upsertedCount } = errResult;
+
+    return {
+      result: { insertedCount, matchedCount, modifiedCount, deletedCount, upsertedCount },
+      error: { code, numWriteErrors: errors.length },
+    };
+  }
+
+  if (result) {
+    const { insertedCount, matchedCount, modifiedCount, deletedCount, upsertedCount } = result;
+
+    return { result: { insertedCount, matchedCount, modifiedCount, deletedCount, upsertedCount } };
+  }
+
+  return undefined;
+};
+
+const getRandomChannels = async (channelColl: Collection, key: string): Promise<Channel[]> => {
+  const channels: Channel[] = [];
+
+  const cursor = channelColl.aggregate([
+    { $match: { channelId: { $regex: `^${key}` }, deletedAt: null } },
+    { $sample: { size: faker.number.int({ min: 0, max: 100 }) } },
+  ]);
+  for await (const doc of cursor) {
+    channels.push(doc as Channel);
+  }
+
+  return channels;
+};
+
+(async () => {
+  const numUsersArg = Number(process.argv[2]) || undefined;
+  const numUsers = Math.min(numUsersArg ?? 100, 1000);
+  logger.info('scripts: addUsersToChannels called', { numUsers });
+
+  const { key, error: keyError } = scriptsHelper.getFakedataKey();
+  if (keyError) {
+    logger.info('scripts: addUsersToChannels exiting', { error: keyError });
+
+    process.exit(1);
+  }
+
+  const { isConnected } = await dbJassbot.connect();
+  if (!isConnected) {
+    logger.info('scripts: addUsersToChannels exiting', { error: 'Database failed to connect' });
+
+    process.exit(1);
+  }
+
+  const channelColl = dbJassbot.getChannelCollection();
+  const userColl = dbJassbot.getUserCollection();
+
+  const channels: { [channelId: string]: { memberIds: string[] } } = {};
+  const userOps: AnyBulkWriteOperation[] = [];
+
+  const userAggCursor = userColl.aggregate([
+    { $match: { deletedAt: null, userId: { $regex: `^${key}` } } },
+    { $sample: { size: numUsers } },
+  ]);
+  for await (const doc of userAggCursor) {
+    const { userId, channelIds = [] } = doc as User;
+    const nowDate = new Date();
+    const randChannels = await getRandomChannels(channelColl, key);
+
+    for (let i = 0; i < randChannels.length; i += 1) {
+      const { channelId, memberIds: randChannelMemberIds = [] } = randChannels[i];
+      if (!channelIds.includes(channelId)) {
+        channelIds.push(channelId);
+      }
+
+      if (channels[channelId]) {
+        const { memberIds } = channels[channelId];
+        if (!memberIds.includes(userId)) {
+          memberIds.push(userId);
+        }
+        channels[channelId] = { memberIds };
+      } else {
+        if (!randChannelMemberIds.includes(userId)) {
+          randChannelMemberIds.push(userId);
+        }
+        channels[channelId] = { memberIds: randChannelMemberIds };
+      }
+    }
+
+    userOps.push({
+      updateOne: {
+        filter: { userId },
+        update: { $set: { channelIds, updatedAt: nowDate }, $setOnInsert: { createdAt: nowDate } },
+        upsert: true,
+      },
+    });
+  }
+
+  const channelOps: AnyBulkWriteOperation[] = [];
+  const randChannelIds = Object.keys(channels);
+  randChannelIds.forEach((channelId) => {
+    const nowDate = new Date();
+    const { memberIds } = channels[channelId];
+
+    channelOps.push({
+      updateOne: {
+        filter: { channelId },
+        update: {
+          $set: { memberIds, numMembers: memberIds.length, updatedAt: nowDate },
+          $setOnInsert: { createdAt: nowDate },
+        },
+        upsert: true,
+      },
+    });
+  });
+
+  const options = { ordered: false };
+  const userBulkWrite = await mongodb.bulkWrite(userColl, userOps, options);
+  logger.info('scripts: addUsersToChannels userBulkWrite completed', getLogContext(userBulkWrite));
+
+  const channelBulkWrite = await mongodb.bulkWrite(channelColl, channelOps, options);
+  logger.info('scripts: addUsersToChannels channelBulkWrite completed', getLogContext(channelBulkWrite));
+
+  await dbJassbot.close();
+
+  process.exit(0);
+})();
